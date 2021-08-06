d0eb8995a2ed3206bab6c1fc19e26c1a99757b65
Max <max@zptx.icu>
2021-08-07 01:22:09 +0800

    correct spelling of milliseconds

--- main.js
+++ main.js
@@ -192,10 +192,10 @@ creator.on("error", err => {
 });
 
 // thanks: https://gist.github.com/flangofas/714f401b63a1c3d84aaa
-function msToTime(miliseconds, format) {
+function msToTime(milliseconds, format) {
     let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
 
-    total_seconds = parseInt(Math.floor(miliseconds / 1000));
+    total_seconds = parseInt(Math.floor(milliseconds / 1000));
     total_minutes = parseInt(Math.floor(total_seconds / 60));
     total_hours = parseInt(Math.floor(total_minutes / 60));
     days = parseInt(Math.floor(total_hours / 24));
