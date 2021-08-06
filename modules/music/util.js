0c0d59ac80010f9c5f76559c7eebdabe5a58e00d
Max <max@zptx.icu>
2021-08-07 01:26:14 +0800

    basic util implementation

--- modules/music/util.js
+++ modules/music/util.js
@@ -1,4 +1,7 @@
 const superagent = require('superagent');
+const getArtistTitle = require('get-artist-title');
+
+const musicGuilds = {};
 
 function querySorter(query) {
     type = "";
@@ -63,6 +66,7 @@ async function resolveTracks(node, search) {
 }
 
 function getPlayer(channel) {
+    const { bot } = require("../../main.js");
     if (!channel || !channel.guild) {
         return Promise.reject('Not a guild channel.');
     }
@@ -77,22 +81,204 @@ function trackHandler(tracks, type, search) {
     if (["LOAD_FAILED", "UNKNOWN", "NO_MATCHES"].includes(tracks.loadType) || tracks.tracks.length == 0) {
         return tracks.loadType;
     }
-    else if (tracks.loadType == "TRACK_LOADED" && ["id", "url"].includes(type)) {
+    else if (tracks.loadType === "TRACK_LOADED" && ["id", "url"].includes(type)) {
         return tracks.tracks[0];
     }
-    else if (tracks.loadType == "SEARCH_RESULT" && ["default", "ytsearch", "scsearch"].includes(type)) {
+    else if (tracks.loadType === "SEARCH_RESULT" && ["default", "ytsearch", "scsearch"].includes(type)) {
         if (!search) {return tracks.tracks[0];}
         else {return tracks.tracks;}
     }
-    else if (tracks.loadType == "PLAYLIST_LOADED" && type == "url") {
+    else if (tracks.loadType === "PLAYLIST_LOADED" && type == "url") {
         return;
     }
     else {
         return "UNKNOWN";
     }
+}
+
+async function queueHandler(track, guild, user, channel, voice) {
+    track.requester = {
+        username: user.username,
+        discriminator: user.discriminator,
+        id: user.id,
+        avatarURL: user.avatarURL,
+        mention: user.mention
+    };
+    track.info.title = track.info.title.length === 0 ? "*Missing title*" : track.info.title;
+    track.info.author = track.info.author.length === 0 ? "*Missing author*" : track.info.author;
+    let newQueue = false;
+    if (!musicGuilds[guild.id] || !musicGuilds[guild.id].queue) {
+        musicGuilds[guild.id] = {
+            queue: [],
+            channel: channel,
+            voice: guild.channels.get(voice),
+            guild: guild,
+            boost: false,
+            volume: 100,
+            loop: false
+        };
+        newQueue = true;
+    }
+    // Queue exists
+    if (typeof musicGuilds[guild.id].queue !== "object") {
+        return {code: "QUEUE_BROKEN"};
+    }
+    let identifiers = musicGuilds[guild.id].queue.map(q => q.info.identifier);
+    if (identifiers.includes(track.info.identifier)) {
+        return {code: "QUEUE_DUPLICATE"};
+    }
+    // There's nothing in there right now
+    if (musicGuilds[guild.id].queue.length === 0) {
+        // Play it
+        play(guild, track.track, newQueue);
+    }
+    let defaultArtist = track.info.author;
+    let defaultTitle = "???";
+    let details = getArtistTitle(track.info.title, {defaultArtist: defaultArtist, defaultTitle: defaultTitle});
+    track.info.friendlyTitle = details[1] !== defaultTitle ? `${details[0]} - ${details[1]}` : null;
+    musicGuilds[guild.id].queue.push(track);
+    return {code: "SUCCESS", track: track, queued: !newQueue};
+}
+
+async function play(guild, track, newQueue) {
+    const { msToTime, msToTimeString, bot } = require("../../main.js");
+    // If no track is found (auto-called after a track ends), set a timeout to auto-disconnect after 30 minutes
+    if (!track) {
+        musicGuilds[guild.id].timeout = setTimeout(guildId => {
+            let channel = musicGuilds[guildId].channel;
+            let voiceId = musicGuilds[guildId].voice.id;
+            delete musicGuilds[guildId];
+            bot.leaveVoiceChannel(voiceId);
+            channel.createMessage({
+                embed: {
+                    description: "Disconnected from inactivity.",
+                    color: 0xf39bff
+                }
+            });
+        }, 1800000, guild.id);
+        return;
+    }
+    // If the playing track is "dc" (requesting a disconnect)
+    else if (track === "dc") {
+        let channel = musicGuilds[guild.id].channel;
+        let voiceId = musicGuilds[guild.id].voice.id;
+        delete musicGuilds[guild.id];
+        bot.leaveVoiceChannel(voiceId);
+        channel.createMessage({
+            embed: {
+                description: "Disconnected by request.",
+                color: 0xf39bff
+            }
+        });
+        return;
+    }
+    // Cancel timeout if there's any set, we're going to be playing something
+    if (musicGuilds[guild.id].timeout) {
+        clearTimeout(musicGuilds[guild.id].timeout);
+        delete musicGuilds[guild.id].timeout;
+    }
+    let player = await getPlayer(musicGuilds[guild.id].voice);
+    if (newQueue) {
+        // Deafen self, because that's cool and quirky (this is purely visual, and doesn't actually stop audio from being received)
+        bot.voiceConnections.get(guild.id).updateVoiceState(musicGuilds[guild.id].voice.id, false, true);
+    }
+    // If there's no one here, excluding bots, we should make a move
+    if (bot.guilds.get(guild.id).channels.get(musicGuilds[guild.id].voice.id).voiceMembers.filter(m => !m.bot).length < 1) {
+        let voice = musicGuilds[guild.id].voice;
+        let channel = musicGuilds[guild.id].channel;
+        delete musicGuilds[guild.id];
+        bot.leaveVoiceChannel(voice.id);
+        channel.createMessage({
+            embed: {
+                description: "Disconnected as everyone left.",
+                color: 0xf39bff
+            }
+        });
+        return;
+    }
+    player.play(track);
+    if (!newQueue) {
+        let currentTrack = musicGuilds[guild.id].queue[0];
+        let durationTime = msToTime(currentTrack.info.length);
+        let duration = msToTimeString(durationTime, true);
+        musicGuilds[guild.id].channel.createMessage({
+            embed: {
+                description: `Now playing **[${currentTrack.info.friendlyTitle === null ? currentTrack.info.title : currentTrack.info.friendlyTitle}](${currentTrack.info.uri})** \`[${duration}]\`\nAdded by ${currentTrack.requester.mention}`,
+                color: 0xf39bff
+            }
+        });
+        return;
+    }
+    player.on("disconnect", err => {
+        if (err) {console.log(err);}
+        delete musicGuilds[guild.id];
+    });
+    player.on("error", async err => {
+        console.log("Error encountered for tracks:");
+        console.log(err);
+        let additionalInfo = "There's no case for what happened, which means something really bad probably happened. Use my disconnect command to reset the session.";
+        if (!musicGuilds[guild.id].errored) {
+            additionalInfo = "Trying again.";
+            let player = await getPlayer(musicGuilds[guild.id].voice);
+            player.play(musicGuilds[guild.id].queue[0].track);
+            musicGuilds[guild.id].errored = true;
+        }
+        else if (musicGuilds[guild.id].errored) {
+            additionalInfo = "Skipping the track.";
+            let voiceId = musicGuilds[guild.id].voice.id;
+            let original = musicGuilds[guild.id].queue;
+            const shifted = original.shift();
+            musicGuilds[guild.id].queue = original;
+            delete musicGuilds[guild.id].skip;
+            delete musicGuilds[guild.id].errored;
+            if (original.length === 0) {next = null;}
+            else {next = original[0].track;}
+            play(guild, next);
+        }
+        musicGuilds[guild.id].channel.createMessage({
+            embed: {
+                description: `An error occurred while playing the track. ${additionalInfo}`,
+                color: 0xf39bff
+            }
+        });
+    });
+    player.on("end", d => {
+        if (d.reason && d.reason === 'REPLACED') {return;}
+        let totalDuration = 0;
+        musicGuilds[guild.id].queue.forEach(track => {
+            if (track.info) {
+                totalDuration += track.info.length;
+            }
+        });
+        let original = musicGuilds[guild.id].queue;
+        const shifted = original.shift();
+        // Loop logic
+        if (musicGuilds[guild.id].loop && shifted) {
+            // Preventing loop if track / queue duration is too short, because this causes ratelimits really quickly
+            if ((original.length === 0 && shifted.info.length < 60000) || totalDuration < 60000) {
+                musicGuilds[guild.id].channel.createMessage({
+                    embed: {
+                        description: `Failed to loop **[${shifted.info.title}](${shifted.info.uri})** as the ${totalDuration < 60000 ? "queue" : "track"} duration is too short.`,
+                        color: 0xf39bff
+                    }
+                });
+            }
+            else {
+                original.push(shifted);
+            }
+        }
+        musicGuilds[guild.id].queue = original;
+        delete musicGuilds[guild.id].skip;
+        delete musicGuilds[guild.id].errored;
+        if (original.length === 0) {next = null;}
+        else {next = original[0].track;}
+        play(guild, next);
+    });
   }
 
+module.exports.musicGuilds = musicGuilds;
 module.exports.querySorter = querySorter;
 module.exports.resolveTracks = resolveTracks;
 module.exports.getPlayer = getPlayer;
 module.exports.trackHandler = trackHandler;
+module.exports.queueHandler = queueHandler;
