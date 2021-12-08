ff968567a2e8cd3b114c3c395bd600f2f06ce42e
Max <max@zptx.icu>
2021-12-08 18:54:04 +0800

    fix: missing event type in error.log

--- main.js
+++ main.js
@@ -501,7 +501,7 @@ async function shuttingDown(eventType, err) {
 	if (err) {
 		console.log('[Quaver] Logging additional output to error.log.');
 		try {
-			await fsPromises.writeFile('error.log', `${err.message}\n${err.stack}`);
+			await fsPromises.writeFile('error.log', `${eventType}\n${err.message}\n${err.stack}`);
 		}
 		catch (e) {
 			console.error(`[Quaver] Encountered error while writing to error.log:\n${e}`);
