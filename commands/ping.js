9e33969ce4f1f6a37659ae235061283e9a79827b
Xayanide <chris.balili.91@gmail.com>
2021-12-09 18:52:10 +0800

    feat: add uptime to footer

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const { token, defaultColor } = require('../settings.json');
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [] })
+const { defaultColor } = require('../settings.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,22 +11,21 @@ module.exports = {
 		user: [],
 		bot: [],
 	},
-	async execute (interaction) {
-        let totalSeconds = (client.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        totalSeconds %= 86400;
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        let minutes = Math.floor(totalSeconds / 60);
-        let seconds = Math.floor(totalSeconds % 60);		
+	async execute(interaction) {
+		let totalSeconds = (interaction.client.uptime / 1000);
+		const days = Math.floor(totalSeconds / 86400);
+		totalSeconds %= 86400;
+		const hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		const minutes = Math.floor(totalSeconds / 60);
+		const seconds = Math.floor(totalSeconds % 60);
 		await interaction.reply({
 			embeds: [
 				new MessageEmbed()
 					.setDescription(`Pong!${interaction.guild ? ` ${interaction.guild.shard.ping}ms` : ''}`)
-                    .setFooter(`Uptime: ${days} d ${hours} h ${minutes} m ${seconds} s`)
-                    .setColor(defaultColor),  
+					.setFooter(`Uptime: ${days} d ${hours} h ${minutes} m ${seconds} s`)
+					.setColor(defaultColor),
 			],
 		});
 	},
 };
-client.login(token);
