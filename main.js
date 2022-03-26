572c39a6fa31bfbf7dc600b99d077fa3fb8b26d9
Xayanide <chris.balili.91@gmail.com>
2022-03-27 03:02:42 +0800

    refactor: add tryCatch on stage start in newState

--- main.js
+++ main.js
@@ -614,7 +614,12 @@ bot.on('voiceStateUpdate', async (oldState, newState) => {
 			}
 			await newState.setSuppressed(false);
 			if (!newState.channel.stageInstance) {
-				await newState.channel.createStageInstance({ topic: getLocale(guildData.get(`${player.guildId}.locale`) ?? defaultLocale, 'MUSIC_STAGE_TOPIC'), privacyLevel: 'GUILD_ONLY' });
+				try {
+					await newState.channel.createStageInstance({ topic: getLocale(guildData.get(`${player.guildId}.locale`) ?? defaultLocale, 'MUSIC_STAGE_TOPIC'), privacyLevel: 'GUILD_ONLY' });
+				}
+				catch (err) {
+					console.error(err);
+				}
 			}
 		}
 		// the new vc has no humans
