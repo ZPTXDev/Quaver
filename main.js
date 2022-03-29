4f0c5d7353b63c09890cf5fd17a9beaccb1147f2
Xayanide <chris.balili.91@gmail.com>
2022-03-29 18:17:19 +0800

    fix: add check on shutdown (fixes #128) (#152)
    * fix: add check on shutdown (fixes #128)
    
    * refactor: use botChannelPerms instead
    
    * fix: allow loop to continue after failed permcheck
    
    Co-authored-by: Max <max@zapteryx.com>

--- main.js
+++ main.js
@@ -778,6 +778,8 @@ async function shuttingDown(eventType, err) {
 			}
 			player.disconnect();
 			bot.music.destroyPlayer(player.guildId);
+			const botChannelPerms = bot.guilds.cache.get(player.guildId).channels.cache.get(player.queue.channel.id).permissionsFor(bot.user.id);
+			if (!botChannelPerms.has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) { continue; }
 			await player.queue.channel.send({
 				embeds: [
 					new MessageEmbed()
