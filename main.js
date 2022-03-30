b1f333c0b2bcdb8ee5351e3538f29e8dc8126111
Max <max@zptx.icu>
2022-03-30 10:05:19 +0800

    feat: display reason when track is skipped

--- main.js
+++ main.js
@@ -199,8 +199,18 @@ bot.music.on('trackStart', async (queue, song) => {
 	});
 });
 
-bot.music.on('trackEnd', queue => {
+bot.music.on('trackEnd', (queue, track, reason) => {
 	delete queue.player.skip;
+	if (reason) {
+		logger.warn({ message: `[G ${queue.player.guildId}] Track skipped with reason: ${reason}`, label: 'Quaver' });
+		queue.channel.send({
+			embeds: [
+				new MessageEmbed()
+					.setDescription(getLocale(guildData.get(`${queue.player.guildId}.locale`) ?? defaultLocale, 'MUSIC_TRACK_SKIPPED', track.title, track.uri, reason))
+					.setColor('DARK_RED'),
+			],
+		});
+	}
 	if (bot.guilds.cache.get(queue.player.guildId).channels.cache.get(queue.player.channelId).members?.filter(m => !m.user.bot).size < 1 && !guildData.get(`${queue.player.guildId}.always.enabled`)) {
 		logger.info({ message: `[G ${queue.player.guildId}] Disconnecting (alone)`, label: 'Quaver' });
 		queue.player.disconnect();
