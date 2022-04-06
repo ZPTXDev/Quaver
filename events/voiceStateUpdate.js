f33cbaed0e009dcaca1ee7a8cecbbbc628db3ff0
Max <max@zapteryx.com>
2022-04-06 16:40:15 +0800

    refactor: remove additional space

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -30,7 +30,7 @@ module.exports = {
 			// channel is a stage channel, and bot is suppressed
 			// this also handles suppressing Quaver mid-track
 			if (newState.channel.type === 'GUILD_STAGE_VOICE' && newState.suppress) {
-				const permissions =	bot.guilds.cache.get(guild.id).channels.cache.get(newState.channelId).permissionsFor(bot.user.id);
+				const permissions = bot.guilds.cache.get(guild.id).channels.cache.get(newState.channelId).permissionsFor(bot.user.id);
 				// check for connect, speak permission for stage channel
 				if (!permissions.has(['VIEW_CHANNEL', 'CONNECT', 'SPEAK'])) {
 					await player.musicHandler.locale('DISCORD_BOT_MISSING_PERMISSIONS_BASIC');
