229fbed9a6a8dc58c84e8ea2d2fa5aae90b41c63
Max <max@zptx.icu>
2022-04-18 15:17:30 +0800

    fix: handle guild leave when playing something Fixes #218

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,6 +4,9 @@ module.exports = {
 	name: 'guildDelete',
 	once: false,
 	execute(guild) {
+		const { bot } = require('../main.js');
 		logger.info({ message: `[G ${guild.id}] Left guild ${guild.name}`, label: 'Discord' });
+		const player = bot.music.players.get(guild.id);
+		if (player) player.musicHandler.disconnect();
 	},
 };
