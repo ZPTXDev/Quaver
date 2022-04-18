99f0293e1d0fba1f9664ef0545ab9b536f366033
Xayanide <chris.balili.91@gmail.com>
2022-04-18 13:05:06 +0800

    refactor: move a variable near a statement

--- classes/MusicHandler.js
+++ classes/MusicHandler.js
@@ -10,11 +10,11 @@ module.exports = class MusicHandler {
 
 	async disconnect() {
 		const { bot } = require('../main.js');
-		const voiceChannel = bot.guilds.cache.get(this.player.guildId).channels.cache.get(this.player.channelId);
 		clearTimeout(this.player.timeout);
 		clearTimeout(this.player.pauseTimeout);
 		this.player.disconnect();
 		bot.music.destroyPlayer(this.player.guildId);
+		const voiceChannel = bot.guilds.cache.get(this.player.guildId).channels.cache.get(this.player.channelId);
 		if (voiceChannel?.type === 'GUILD_STAGE_VOICE') {
 			const permissions = bot.guilds.cache.get(this.player.guildId).channels.cache.get(this.player.channelId).permissionsFor(bot.user.id);
 			if (!permissions.has(['VIEW_CHANNEL', 'CONNECT', 'SPEAK'])) {
