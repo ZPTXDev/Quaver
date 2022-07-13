3401e8e6cb4d46aebe459a359469866181b03d15
Max <max@zapteryx.com>
2022-07-13 16:39:02 +0800

    fix: rare pause timeout bug (#329)
    * fix: rare pause timeout bug
    
    * fix: attempt fix for queueFinish as well

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -161,6 +161,8 @@ module.exports = {
 			await player.handler.disconnect();
 			return;
 		}
+		// rare case where the bot sets pause timeout after setting timeout
+		if (player.timeout) return;
 		await player.pause();
 		logger.info({ message: `[G ${player.guildId}] Setting pause timeout`, label: 'Quaver' });
 		if (player.pauseTimeout) {
