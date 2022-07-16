eac1b5c09eb0ef57e70400f200e2ed2b39d914a1
Xayanide <chris.balili.91@gmail.com>
2022-07-16 19:00:18 +0800

    refactor: add missing await to PlayerHandler method invokers (#351)
    Closes #347

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,10 +4,10 @@ module.exports = {
 	name: 'guildDelete',
 	once: false,
 	/** @param {import('discord.js').Guild} guild */
-	execute(guild) {
+	async execute(guild) {
 		const { bot } = require('../main.js');
 		logger.info({ message: `[G ${guild.id}] Left guild ${guild.name}`, label: 'Discord' });
 		const player = bot.music.players.get(guild.id);
-		if (player) player.handler.disconnect();
+		if (player) await player.handler.disconnect();
 	},
 };
