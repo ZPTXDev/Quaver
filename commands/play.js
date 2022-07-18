e91237208494b149deffcfcf56858c55cee6c1c9
Xayanide <chris.balili.91@gmail.com>
2022-07-18 20:05:51 +0800

    refactor: add optional chaining operators as failsafes

--- commands/play.js
+++ commands/play.js
@@ -106,7 +106,7 @@ module.exports = {
 			await player.connect(interaction.member.voice.channelId, { deafened: true });
 			// Ensure that Quaver destroys the player if the user leaves the channel while Quaver is queueing tracks
 			// Ensure that Quaver destroys the player if Quaver gets timed out by the user while Quaver is queueing tracks
-			if (!interaction.member.voice.channelId || interaction.guild.members.me.isCommunicationDisabled()) {
+			if (!interaction.member.voice.channelId || interaction.guild?.members.me.isCommunicationDisabled()) {
 				await interaction.replyHandler.locale('DISCORD_INTERACTION_CANCELED', {}, interaction.user.id);
 				await player.handler.disconnect();
 				return;
