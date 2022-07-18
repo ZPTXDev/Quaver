466da58ad7ba56589d6524fc439d3cf710808a0b
Max <max@zptx.dev>
2022-07-18 12:27:50 +0800

    fix(command-info): update reference

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { Permissions } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const { version } = require('../package.json');
 const { defaultLocale } = require('../settings.json');
 const { getLocale } = require('../functions.js');
@@ -15,6 +14,6 @@ module.exports = {
 	},
 	/** @param {import('discord.js').CommandInteraction & {client: import('discord.js').Client, replyHandler: import('../classes/ReplyHandler.js')}} interaction */
 	async execute(interaction) {
-		await interaction.replyHandler.locale('CMD_INFO_DETAIL', { title: 'Quaver', thumbnail: interaction.client.user.avatarURL({ format: 'png' }) }, interaction.client.generateInvite({ permissions: [Permissions.FLAGS.ADMINISTRATOR], scopes: ['bot', 'applications.commands'] }), version);
+		await interaction.replyHandler.locale('CMD_INFO_DETAIL', { title: 'Quaver', thumbnail: interaction.client.user.avatarURL({ format: 'png' }) }, interaction.client.generateInvite({ permissions: [PermissionsBitField.Flags.Administrator], scopes: ['bot', 'applications.commands'] }), version);
 	},
 };
