a101f908b726527d1c3ad0f80c67945be15a4a5b
Xayanide <chris.balili.91@gmail.com>
2022-07-18 19:06:34 +0800

    docs: elaborate

--- commands/play.js
+++ commands/play.js
@@ -105,7 +105,7 @@ module.exports = {
 			player.queue.channel = interaction.channel;
 			await player.connect(interaction.member.voice.channelId, { deafened: true });
 			// Ensure that Quaver destroys the player if the user leaves the channel while Quaver is queueing tracks
-			// Ensure that Quaver destroys the player if Quaver gets timed out while queueing tracks
+			// Ensure that Quaver destroys the player if Quaver gets timed out by the user while Quaver is queueing tracks
 			if (!interaction.member.voice.channelId || interaction.guild.members.me.isCommunicationDisabled()) {
 				await interaction.replyHandler.locale('DISCORD_INTERACTION_CANCELED', {}, interaction.user.id);
 				await player.handler.disconnect();
