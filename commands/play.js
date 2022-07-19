47b1068c163db01224450458dcfc7fcd01e4f3b2
Xayanide <chris.balili.91@gmail.com>
2022-07-19 22:10:15 +0800

    fix: destroy player if no guild while queuing tracks
    Fixes #392

--- commands/play.js
+++ commands/play.js
@@ -106,9 +106,10 @@ module.exports = {
 			await player.connect(interaction.member.voice.channelId, { deafened: true });
 			// Ensure that Quaver destroys the player if the user leaves the channel while Quaver is queueing tracks
 			// Ensure that Quaver destroys the player if Quaver gets timed out by the user while Quaver is queueing tracks
+			// Ensure that Quaver destroys the player if Quaver gets kicked or banned by the user while Quaver is queueing tracks
 			const timedOut = interaction.guild?.members.me.isCommunicationDisabled();
-			if (!interaction.member.voice.channelId || timedOut) {
-				timedOut ? await interaction.replyHandler.localeError('DISCORD_BOT_TIMED_OUT') : await interaction.replyHandler.locale('DISCORD_INTERACTION_CANCELED', {}, interaction.user.id);
+			if (!interaction.member.voice.channelId || timedOut || !interaction.guild) {
+				if (interaction.guild) timedOut ? await interaction.replyHandler.localeError('DISCORD_BOT_TIMED_OUT') : await interaction.replyHandler.locale('DISCORD_INTERACTION_CANCELED', {}, interaction.user.id);
 				await player.handler.disconnect();
 				return;
 			}
