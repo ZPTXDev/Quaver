9b77153cea6114d852ed16ae29d36379153735ef
Xayanide <chris.balili.91@gmail.com>
2022-07-30 05:11:28 +0800

    refactor(guildDelete): useless async

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'guildDelete',
 	once: false,
 	/** @param {import('discord.js').Guild} guild */
-	async execute(guild) {
+	execute(guild) {
 		logger.info({ message: `[G ${guild.id}] Left guild ${guild.name}`, label: 'Discord' });
 	},
 };
