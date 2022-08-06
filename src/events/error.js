c27982db873cf42477d209bcd9fa5bdb913db19c
Xayanide <chris.balili.91@gmail.com>
2022-08-06 21:28:29 +0800

    refactor(event-error): shut down on Client#error
    Closes #421

--- src/events/error.js
+++ src/events/error.js
@@ -1,10 +1,12 @@
 const { logger } = require('#lib/util/common.js');
+const { shuttingDown } = require('#src/main.js');
 
 module.exports = {
 	name: 'error',
 	once: false,
 	/** @param {Error} err */
-	execute(err) {
+	async execute(err) {
 		logger.error({ message: `${err.message}\n${err.stack}`, label: 'Quaver' });
+		await shuttingDown('discord', err);
 	},
 };
