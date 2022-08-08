4a8572e2d4995cd2f6a9600bd48739cdff456e9d
Max <max@zapteryx.com>
2022-08-08 18:43:37 +0800

    fix(cmd-pause): use correct locale
    Co-authored-by: Xayanide <chris.balili.91@gmail.com>

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -16,7 +16,7 @@ export default {
 	async execute(interaction) {
 		const player = interaction.client.music.players.get(interaction.guildId);
 		if (player.paused) {
-			await interaction.replyHandler.locale('CMD.RESPONSE.PAUSE.STATE_UNCHANGED', {}, 'error');
+			await interaction.replyHandler.locale('CMD.PAUSE.RESPONSE.STATE_UNCHANGED', {}, 'error');
 			return;
 		}
 		player.pause();
