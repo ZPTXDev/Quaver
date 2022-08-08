e4358ce2ed80ab3f09256a7a42307f44cd79ef46
Xayanide <chris.balili.91@gmail.com>
2022-08-09 02:44:51 +0800

    refactor(event-error): shut down on Client#error and update log message (#430)
    * refactor(event-error): shut down on Client#error
    
    Closes #421
    
    * refactor(event-error): update logger#error message

--- src/events/error.js
+++ src/events/error.js
@@ -1,10 +1,12 @@
 import { logger } from '#lib/util/common.js';
+import { shuttingDown } from '#src/main.js';
 
 export default {
 	name: 'error',
 	once: false,
 	/** @param {Error} err */
-	execute(err) {
-		logger.error({ message: `${err.message}\n${err.stack}`, label: 'Quaver' });
+	async execute(err) {
+		logger.error({ message: 'An error occurred. Quaver will now shut down to prevent any further issues.', label: 'Discord' });
+		await shuttingDown('discord', err);
 	},
 };
