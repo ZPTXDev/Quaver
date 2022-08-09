c4cc0f43b3ff11375223d81bb6d32d348f76ff57
Xayanide <chris.balili.91@gmail.com>
2022-08-09 11:46:53 +0800

    fix(event-error): circular import (#441)
    Fixes #440

--- src/events/error.js
+++ src/events/error.js
@@ -1,11 +1,11 @@
 import { logger } from '#lib/util/common.js';
-import { shuttingDown } from '#src/main.js';
 
 export default {
 	name: 'error',
 	once: false,
 	/** @param {Error} err */
 	async execute(err) {
+		const { shuttingDown } = await import('#src/main.js');
 		logger.error({ message: 'An error occurred. Quaver will now shut down to prevent any further issues.', label: 'Discord' });
 		await shuttingDown('discord', err);
 	},
