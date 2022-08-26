2397c62ee562f8dc9d45eea9adfd53e3119305de
Max <max@zptx.dev>
2022-08-27 02:25:25 +0800

    refactor(cmd-remove): autocomplete style

--- src/autocomplete/remove.js
+++ src/autocomplete/remove.js
@@ -1,9 +1,9 @@
 export default {
 	name: 'remove',
 	async execute(interaction) {
+		const focused = interaction.options.getFocused();
 		const player = interaction.client.music.players.get(interaction.guildId);
 		if (!player) return interaction.respond([]);
-		const focused = interaction.options.getFocused();
 		return interaction.respond(
 			player.queue.tracks
 				.map((track, index) => ({ name: `${index + 1}. ${track.title}`, value: index + 1, title: track.title, requester: track.requester }))
