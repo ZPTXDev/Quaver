1e35126bd96ea0b532e30749e377553a668cb660
Max <max@zptx.dev>
2022-08-26 21:00:37 +0800

    fix(cmd-remove): use lowercase for autocomplete

--- src/autocomplete/remove.js
+++ src/autocomplete/remove.js
@@ -7,7 +7,7 @@ export default {
 		await interaction.respond(
 			player.queue.tracks
 				.map((track, index) => ({ name: `${index + 1}. ${track.title}`, value: index, title: track.title, requester: track.requester }))
-				.filter(track => track.requester === interaction.user.id && track.title.startsWith(focused))
+				.filter(track => track.requester === interaction.user.id && track.title.toLowerCase().startsWith(focused.toLowerCase()))
 				.map(track => ({ name: track.name, value: track.value })),
 		);
 	},
