f97ed598e9e2e18c93290f1e1208a636cda8daec
Max <max@zptx.dev>
2022-08-27 01:06:48 +0800

    fix(cmd-remove): value outside range error

--- src/autocomplete/remove.js
+++ src/autocomplete/remove.js
@@ -6,7 +6,7 @@ export default {
 		const focused = interaction.options.getFocused();
 		await interaction.respond(
 			player.queue.tracks
-				.map((track, index) => ({ name: `${index + 1}. ${track.title}`, value: index, title: track.title, requester: track.requester }))
+				.map((track, index) => ({ name: `${index + 1}. ${track.title}`, value: index + 1, title: track.title, requester: track.requester }))
 				.filter(track => track.requester === interaction.user.id && track.title.toLowerCase().startsWith(focused.toLowerCase()))
 				.map(track => ({ name: track.name, value: track.value })),
 		);
