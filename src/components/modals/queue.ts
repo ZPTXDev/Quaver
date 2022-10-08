a545f8d8874a6e955f2c9feb55e8bb2427c54a0a
Xayanide <chris.balili.91@gmail.com>
2022-10-09 07:11:30 +0800

    fix(ReplyHandler#reply): edit the ephemeral with toast if there are no pages
    Fixes #527

--- src/components/modals/queue.ts
+++ src/components/modals/queue.ts
@@ -14,7 +14,7 @@ export default {
 			return;
 		}
 		if (player) pages = paginate(player.queue.tracks, 5);
-		if (!player) {
+		if (!player || pages) {
 			await interaction.replyHandler.locale('CMD.QUEUE.RESPONSE.QUEUE_EMPTY', { type: 'error', components: [], force: 'update' });
 			return;
 		}
