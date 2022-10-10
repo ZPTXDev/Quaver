191bbf3c30ec2c87ba5250f5b5310810dd51e2f0
Max <max@zptx.dev>
2022-10-10 22:34:21 +0800

    refactor: remove diff stuff

--- src/lib/util/common.d.ts
+++ src/lib/util/common.d.ts
@@ -5,17 +5,17 @@ import type PlayerHandler from '../PlayerHandler.js';
 import type ReplyHandler from '../ReplyHandler.js';
 
 export type SearchStateRecord = {
-    pages: { info: Song }[][];
-    timeout: ReturnType<typeof setTimeout>;
-    selected: Snowflake[];
+	pages: { info: Song }[][];
+	timeout: ReturnType<typeof setTimeout>;
+	selected: Snowflake[];
 };
 
 export type SettingsPageOptions = 'language' | 'format';
 
 export type SettingsPage = {
-    current: string;
-    embeds: EmbedBuilder[];
-    actionRow: ActionRowBuilder;
+	current: string;
+	embeds: EmbedBuilder[];
+	actionRow: ActionRowBuilder;
 };
 
 export type MessageOptionsBuilderInputs = string | EmbedBuilder | (string | EmbedBuilder)[];
@@ -31,46 +31,46 @@ export type JSONResponse<T> = { message?: string } & T;
 export type QuaverChannels = TextChannel | VoiceChannel;
 
 export type QuaverClient = Client<boolean> & {
-    music?: Node;
-+    commands: Collection<string, ChatInputCommand>;
-+    buttons: Collection<string, Button>;
-+    selectmenus: Collection<string, SelectMenu>;
-+    autocomplete: Collection<string, Autocomplete>;
-+    modals: Collection<string, ModalSubmit>;
+	music?: Node;
+	commands: Collection<string, ChatInputCommand>;
+	buttons: Collection<string, Button>;
+	selectmenus: Collection<string, SelectMenu>;
+	autocomplete: Collection<string, Autocomplete>;
+	modals: Collection<string, ModalSubmit>;
 };
 
 export type QuaverSong = Song & {
-    requesterTag?: string;
+	requesterTag?: string;
 };
 
 export type QuaverPlayer = Player<Node> & {
-    timeout?: ReturnType<typeof setTimeout>;
-    pauseTimeout?: ReturnType<typeof setTimeout>;
-    timeoutEnd?: number;
-    queue: QuaverQueue;
-    bassboost: boolean;
-    nightcore: boolean;
-    handler: PlayerHandler;
-    skip: QuaverPlayerSkipObject;
-    failed: number;
+	timeout?: ReturnType<typeof setTimeout>;
+	pauseTimeout?: ReturnType<typeof setTimeout>;
+	timeoutEnd?: number;
+	queue: QuaverQueue;
+	bassboost: boolean;
+	nightcore: boolean;
+	handler: PlayerHandler;
+	skip: QuaverPlayerSkipObject;
+	failed: number;
 };
 
 export type QuaverQueue = Queue & {
-    channel?: QuaverChannels;
-    player: QuaverPlayer;
-    current: QuaverSong;
+	channel?: QuaverChannels;
+	player: QuaverPlayer;
+	current: QuaverSong;
 };
 
 export type QuaverPlayerSkipObject = {
-    required: number;
-    users: Snowflake[];
+	required: number;
+	users: Snowflake[];
 };
 
 export type QuaverInteraction<T> =
-    T extends AutocompleteInteraction ? AutocompleteInteraction & {
-        client: QuaverClient;
-    } :
-    T & {
-        client: QuaverClient;
-        replyHandler: ReplyHandler;
-    };
+	T extends AutocompleteInteraction ? AutocompleteInteraction & {
+		client: QuaverClient;
+	} :
+	T & {
+		client: QuaverClient;
+		replyHandler: ReplyHandler;
+	};
