d5cccb577851d338c91987f9a63e7e1e003f4cf0
Max <max@zapteryx.com>
2022-10-13 09:44:10 +0800

    refactor: more stuff
    if this breaks i'm not responsible
    
    Co-authored-by: Xayanide <chris.balili.91@gmail.com>

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,6 +1,14 @@
+import type { ClientEvents } from 'discord.js';
+import type { NodeEvents } from 'lavaclient';
+
 export type QuaverEvent = {
-    name: string;
+    name: keyof ClientEvents
     once: boolean;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    execute(...args: any[]): void | Promise<void>;
+    execute<K extends keyof ClientEvents>(...args: ClientEvents[K]): void | Promise<void>;
 };
+
+export type QuaverMusicEvent = {
+    name: keyof NodeEvents;
+    once: boolean;
+    execute<K extends NodeEvents>(...args: NodeEvents[K]): void | Promise<void>;
+}
