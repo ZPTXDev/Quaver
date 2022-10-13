8e4a4e7b8a3d776ac4ef619e1cade8464ef88175
Max <max@zptx.dev>
2022-10-14 00:09:04 +0800

    refactor: specify type and rename variable

--- src/autocomplete/search.ts
+++ src/autocomplete/search.ts
@@ -11,16 +11,16 @@ export default {
 		try {
 			data = await body.text();
 			let searchSuggestions: string[] = [];
-			data.split('[').forEach((ele, index): void => {
-				if (!ele.split('"')[1] || index === 1) return;
-				searchSuggestions.push(ele.split('"')[1]);
+			data.split('[').forEach((element: string, index: number): void => {
+				if (!element.split('"')[1] || index === 1) return;
+				searchSuggestions.push(element.split('"')[1]);
 			});
 			searchSuggestions.pop();
-			searchSuggestions = searchSuggestions.filter((ele): boolean => ele !== focused);
+			searchSuggestions = searchSuggestions.filter((element): boolean => element !== focused);
 			searchSuggestions.unshift(focused);
 			return interaction.respond(
 				searchSuggestions
-					.filter((ele): boolean => ele !== '')
+					.filter((element): boolean => element !== '')
 					.map((suggestion): string => suggestion.replace(/\\u([0-9a-fA-F]{4})/g, (whole, grp): string => String.fromCharCode(parseInt(grp, 16))))
 					.filter((suggestion): boolean => suggestion.length <= 100)
 					.map((suggestion): ApplicationCommandOptionChoiceData => ({ name: suggestion, value: suggestion })),
