17e93acb0344efa46d356b04b5f2449c48a766e1
Max <max@zptx.dev>
2022-10-24 11:57:03 +0800

    fix: avoid shutting down if "discord" error

--- src/events/error.ts
+++ src/events/error.ts
@@ -3,13 +3,10 @@ import { logger } from '#src/lib/util/common.js';
 export default {
     name: 'error',
     once: false,
-    async execute(err: Error): Promise<void> {
-        const { shuttingDown } = await import('#src/main.js');
+    execute(err: Error): void {
         logger.error({
-            message:
-                'An error occurred. Quaver will now shut down to prevent any further issues.',
-            label: 'Discord',
+            message: `${err.message}\n${err.stack}`,
+            label: 'Quaver',
         });
-        await shuttingDown('discord', err);
     },
 };
