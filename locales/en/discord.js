dded3b7d8a93d7dc8e2e313e29b78967ea562f0a
Max <max@zptx.dev>
2022-10-27 19:27:02 +0800

    refactor: use bold markdown for permissions

--- locales/en/discord.js
+++ locales/en/discord.js
@@ -3,9 +3,9 @@ export default {
 		USER: 'You are missing permission(s): %1',
 		BOT: {
 			DEFAULT: 'I am missing permission(s): %1',
-			VIEW: 'I need the View Channel and Send Messages permissions in this channel.',
-			BASIC: 'I need the Connect and Speak permissions in the voice channel.',
-			STAGE: 'I need to be a stage moderator in the stage channel.',
+			VIEW: 'I need the **View Channel** and **Send Messages** permissions in this channel.',
+			BASIC: 'I need the **Connect** and **Speak** permissions in the voice channel.',
+			STAGE: 'I need to be a **Stage Moderator** in the stage channel.',
 			TIMED_OUT: 'I am currently timed out.'
 		}
 	},
