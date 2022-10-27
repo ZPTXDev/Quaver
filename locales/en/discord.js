7a4809624cbd0acf3a6ce71a48ce647630de4d9d
Max <max@zptx.dev>
2022-10-27 19:09:51 +0800

    refactor: add new string for permissions, make strings consistent

--- locales/en/discord.js
+++ locales/en/discord.js
@@ -3,7 +3,8 @@ export default {
 		USER: 'You are missing permission(s): %1',
 		BOT: {
 			DEFAULT: 'I am missing permission(s): %1',
-			BASIC: 'I need to be able to connect and speak in the voice channel.',
+			VIEW: 'I need the View Channel and Send Messages permissions in this channel.',
+			BASIC: 'I need the Connect and Speak permissions in the voice channel.',
 			STAGE: 'I need to be a stage moderator in the stage channel.',
 			TIMED_OUT: 'I am currently timed out.'
 		}
