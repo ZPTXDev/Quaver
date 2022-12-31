622b28b31ef0c5a380ccb5ac760675688c57acbe
Max <max@zptx.dev>
2022-12-31 14:25:58 +0800

    refactor: update types to include new attributes

--- src/events/web/fetchguilds.d.ts
+++ src/events/web/fetchguilds.d.ts
@@ -1,3 +1,8 @@
 import type { APIGuild } from 'discord.js';
 
-export type WebGuild = APIGuild & { botInGuild?: boolean };
+export type WebGuild = APIGuild & {
+    botInGuild?: boolean;
+    idle?: boolean;
+    track?: string;
+    premium?: boolean;
+};
