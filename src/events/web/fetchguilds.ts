8e155f1db391c9e51858412c49ea38391f9ad108
Max <max@zptx.dev>
2022-12-31 14:26:33 +0800

    refactor(web): update fetchguilds to include new attributes

--- src/events/web/fetchguilds.ts
+++ src/events/web/fetchguilds.ts
@@ -30,15 +30,54 @@ export default {
                 },
             },
         );
-        let response = (await getJSONResponse(guilds.body)) as JSONResponse<
+        const response = (await getJSONResponse(guilds.body)) as JSONResponse<
             WebGuild[]
         >;
         if (response.message) return callback({ status: 'error-auth' });
-        response = response.map((guild): WebGuild => {
+        const webGuilds = response.map((guild): WebGuild => {
             guild.botInGuild = !!bot.guilds.cache.get(guild.id);
+            const player = guild.botInGuild && bot.music.players.get(guild.id);
+            guild.idle =
+                guild.botInGuild && player
+                    ? !player.queue.current ||
+                      (!player.playing && !player.paused)
+                    : true;
+            guild.track =
+                guild.botInGuild && !guild.idle
+                    ? player.queue.current.title
+                    : '';
             return guild;
         });
-        socket.guilds = response;
-        return callback({ status: 'success', guilds: response, version });
+        if (settings.premiumURL) {
+            for (const webGuild of webGuilds) {
+                if (!webGuild.botInGuild) continue;
+                for (const feature of [
+                    'stay',
+                    'autolyrics',
+                    'smartqueue',
+                ].filter(
+                    (feat: WhitelistedFeatures): boolean =>
+                        settings.features[feat].enabled &&
+                        settings.features[feat].whitelist &&
+                        settings.features[feat].premium,
+                )) {
+                    const whitelisted = await getGuildFeatureWhitelisted(
+                        webGuild.id,
+                        feature as WhitelistedFeatures,
+                    );
+                    if (
+                        ![
+                            WhitelistStatus.NotWhitelisted,
+                            WhitelistStatus.Expired,
+                        ].includes(whitelisted)
+                    ) {
+                        webGuild.premium = true;
+                        break;
+                    }
+                }
+            }
+        }
+        socket.guilds = webGuilds;
+        return callback({ status: 'success', guilds: webGuilds, version });
     },
 };
