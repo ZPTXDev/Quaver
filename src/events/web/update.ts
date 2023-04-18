ad93e2d6947d15a3d6da4d9f253bdec03bb35d60
Max <max@zptx.dev>
2023-04-18 12:24:43 +0800

    feat(web): descriptive error messages provide more descriptive error messages to web, allowing more helpful messages to be displayed to users. also tidies up the code a little, although a full rewrite is intermittent

--- src/events/web/update.ts
+++ src/events/web/update.ts
@@ -24,9 +24,11 @@ export default {
         item: { type: UpdateItemType; value?: any },
     ): Promise<void> {
         const { bot, io } = await import('#src/main.js');
-        if (!socket.guilds) return callback({ status: 'error-auth' });
+        if (!socket.guilds) {
+            return callback({ status: Response.AuthenticationError });
+        }
         if (!socket.guilds.find((guild): boolean => guild.id === guildId)) {
-            return callback({ status: 'error-auth' });
+            return callback({ status: Response.AuthenticationError });
         }
         if (
             ![
@@ -38,41 +40,46 @@ export default {
                 ?.voice.channelId !==
                 bot.guilds.cache.get(guildId).members.me.voice.channelId
         ) {
-            return callback({ status: 'error-generic' });
+            return callback({ status: Response.ChannelMismatchError });
         }
         switch (item.type) {
             case UpdateItemType.Loop: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
-                const response = await player.handler.loop(item.value);
-                if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
                 }
+                await player.handler.loop(item.value);
                 break;
             }
             case UpdateItemType.Volume: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const response = await player.handler.volume(item.value);
                 if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 break;
             }
             case UpdateItemType.Paused: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const response = item.value
                     ? await player.handler.pause()
                     : await player.handler.resume();
                 if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 break;
             }
             case UpdateItemType.Skip: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const requesterStatus = await getRequesterStatus(
                     player.queue.current,
                     bot.guilds.cache
@@ -83,7 +90,7 @@ export default {
                 if (requesterStatus !== RequesterStatus.NotRequester) {
                     const response = await player.handler.skip();
                     if (response !== PlayerResponse.Success) {
-                        return callback({ status: 'error-generic' });
+                        return callback({ status: Response.GenericError });
                     }
                     break;
                 }
@@ -98,13 +105,13 @@ export default {
                     users: [],
                 };
                 if (skip.users.includes(socket.user.id)) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 skip.users.push(socket.user.id);
                 if (skip.users.length >= skip.required) {
                     const response = await player.handler.skip();
                     if (response !== PlayerResponse.Success) {
-                        return callback({ status: 'error-generic' });
+                        return callback({ status: Response.GenericError });
                     }
                     break;
                 }
@@ -113,25 +120,25 @@ export default {
             }
             case UpdateItemType.Bassboost: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
-                const response = await player.handler.bassboost(item.value);
-                if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
                 }
+                await player.handler.bassboost(item.value);
                 break;
             }
             case UpdateItemType.Nightcore: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
-                const response = await player.handler.nightcore(item.value);
-                if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
                 }
+                await player.handler.nightcore(item.value);
                 break;
             }
             case UpdateItemType.Seek: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const requesterStatus = await getRequesterStatus(
                     player.queue.current,
                     bot.guilds.cache
@@ -140,38 +147,55 @@ export default {
                     player.queue.channel,
                 );
                 if (requesterStatus === RequesterStatus.NotRequester) {
-                    return callback({ status: 'error-auth' });
+                    return callback({ status: Response.AuthenticationError });
                 }
                 const response = await player.handler.seek(item.value);
                 if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 break;
             }
             case UpdateItemType.Remove: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const response = await player.handler.remove(item.value + 1);
                 if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 break;
             }
             case UpdateItemType.Shuffle: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const response = await player.handler.shuffle();
                 if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                    return callback({ status: Response.GenericError });
                 }
                 break;
             }
             case UpdateItemType.StayFeature: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
-                if (!player) return callback({ status: 'error-generic' });
+                if (!player) {
+                    return callback({ status: Response.InactiveSessionError });
+                }
                 const response = await player.handler.stay(item.value);
-                if (response !== PlayerResponse.Success) {
-                    return callback({ status: 'error-generic' });
+                switch (response) {
+                    case PlayerResponse.FeatureDisabled:
+                        return callback({
+                            status: Response.FeatureDisabledError,
+                        });
+                    case PlayerResponse.FeatureNotWhitelisted:
+                        return callback({
+                            status: Response.FeatureNotWhitelistedError,
+                        });
+                    case PlayerResponse.QueueChannelMissing:
+                        return callback({
+                            status: Response.InactiveSessionError,
+                        });
                 }
                 break;
             }
@@ -184,11 +208,13 @@ export default {
                             PermissionsBitField.Flags.ManageGuild,
                         )
                 ) {
-                    return callback({ status: 'error-auth' });
+                    return callback({ status: Response.AuthenticationError });
                 }
                 if (item.value) {
                     if (!settings.features.autolyrics.enabled) {
-                        return callback({ status: 'error-generic' });
+                        return callback({
+                            status: Response.FeatureDisabledError,
+                        });
                     }
                     const whitelisted = await getGuildFeatureWhitelisted(
                         guildId,
@@ -198,7 +224,9 @@ export default {
                         whitelisted === WhitelistStatus.NotWhitelisted ||
                         whitelisted === WhitelistStatus.Expired
                     ) {
-                        return callback({ status: 'error-generic' });
+                        return callback({
+                            status: Response.FeatureNotWhitelistedError,
+                        });
                     }
                 }
                 await data.guild.set(
@@ -226,7 +254,9 @@ export default {
                 }
                 if (item.value) {
                     if (!settings.features.smartqueue.enabled) {
-                        return callback({ status: 'error-generic' });
+                        return callback({
+                            status: Response.FeatureDisabledError,
+                        });
                     }
                     const whitelisted = await getGuildFeatureWhitelisted(
                         guildId,
@@ -236,7 +266,9 @@ export default {
                         whitelisted === WhitelistStatus.NotWhitelisted ||
                         whitelisted === WhitelistStatus.Expired
                     ) {
-                        return callback({ status: 'error-generic' });
+                        return callback({
+                            status: Response.FeatureNotWhitelistedError,
+                        });
                     }
                 }
                 await data.guild.set(
@@ -252,7 +284,7 @@ export default {
                 break;
             }
         }
-        return callback({ status: 'success' });
+        return callback({ status: Response.Success });
     },
 };
 
@@ -269,4 +301,14 @@ export enum UpdateItemType {
     StayFeature = 'stayFeature',
     AutoLyricsFeature = 'autoLyricsFeature',
     SmartQueueFeature = 'smartQueueFeature',
+}
+
+enum Response {
+    Success = 'success',
+    AuthenticationError = 'error-auth',
+    GenericError = 'error-generic',
+    ChannelMismatchError = 'error-channel-mismatch',
+    InactiveSessionError = 'error-inactive-session',
+    FeatureDisabledError = 'error-feature-disabled',
+    FeatureNotWhitelistedError = 'error-feature-not-whitelisted',
 }
