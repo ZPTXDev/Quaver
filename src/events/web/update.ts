e2487ec53b0bfe79dc32847ac355c92d30a6ec14
Max <max@zptx.dev>
2023-04-18 15:30:20 +0800

    feat(web): initial support for adding tracks through web

--- src/events/web/update.ts
+++ src/events/web/update.ts
@@ -1,15 +1,21 @@
-import { PlayerResponse } from '#src/lib/PlayerHandler.js';
-import type { QuaverPlayer } from '#src/lib/util/common.d.js';
+import PlayerHandler, { PlayerResponse } from '#src/lib/PlayerHandler.js';
+import type {
+    QuaverChannels,
+    QuaverPlayer,
+    QuaverSong,
+} from '#src/lib/util/common.d.js';
 import { data } from '#src/lib/util/common.js';
 import { settings } from '#src/lib/util/settings.js';
 import {
-    getGuildFeatureWhitelisted,
-    getRequesterStatus,
     RequesterStatus,
     WhitelistStatus,
+    getGuildFeatureWhitelisted,
+    getRequesterStatus,
 } from '#src/lib/util/util.js';
-import type { APIGuild, APIUser, GuildMember, Snowflake } from 'discord.js';
-import { PermissionsBitField } from 'discord.js';
+import type { Item } from '@lavaclient/spotify';
+import { SpotifyItemType } from '@lavaclient/spotify';
+import type { APIGuild, APIUser, Snowflake } from 'discord.js';
+import { ChannelType, GuildMember, PermissionsBitField } from 'discord.js';
 import type { Socket } from 'socket.io';
 
 export default {
@@ -43,6 +49,160 @@ export default {
             return callback({ status: Response.ChannelMismatchError });
         }
         switch (item.type) {
+            case UpdateItemType.Add: {
+                const member = await bot.guilds.cache
+                    .get(guildId)
+                    .members.fetch(socket.user.id);
+                if (!(member instanceof GuildMember)) {
+                    return callback({ status: Response.GenericError });
+                }
+                if (member.voice.channel.type !== ChannelType.GuildVoice) {
+                    return callback({
+                        status: Response.ChannelUnsupportedError,
+                    });
+                }
+                const permissions = member.voice.channel.permissionsFor(
+                    bot.user.id,
+                );
+                if (
+                    !permissions.has(
+                        new PermissionsBitField([
+                            PermissionsBitField.Flags.ViewChannel,
+                            PermissionsBitField.Flags.Connect,
+                            PermissionsBitField.Flags.Speak,
+                        ]),
+                    )
+                ) {
+                    return callback({ status: Response.BotPermissionError });
+                }
+                let me = await bot.guilds.cache.get(guildId).members.fetchMe();
+                if (me.isCommunicationDisabled()) {
+                    return callback({ status: Response.BotTimedOutError });
+                }
+                let query = item.value;
+                let tracks = [];
+                if (
+                    bot.music.spotify.isSpotifyUrl(query.replace('embed/', ''))
+                ) {
+                    query = query.replace('embed/', '');
+                    if (
+                        !settings.features.spotify.enabled ||
+                        !settings.features.spotify.client_id ||
+                        !settings.features.spotify.client_secret
+                    ) {
+                        return callback({
+                            status: Response.FeatureDisabledError,
+                        });
+                    }
+                    let spotifyItem: Item;
+                    try {
+                        spotifyItem = await bot.music.spotify.load(query);
+                    } catch (err) {
+                        return callback({ status: Response.NoResultsError });
+                    }
+                    switch (spotifyItem?.type) {
+                        case SpotifyItemType.Track: {
+                            const track =
+                                await spotifyItem.resolveYoutubeTrack();
+                            tracks = [track];
+                            break;
+                        }
+                        case SpotifyItemType.Album:
+                        case SpotifyItemType.Playlist:
+                        case SpotifyItemType.Artist:
+                            if (
+                                (spotifyItem.type === SpotifyItemType.Artist
+                                    ? spotifyItem.topTracks
+                                    : spotifyItem.tracks
+                                ).length > 500
+                            ) {
+                                return callback({
+                                    status: Response.SpotifyTooManyTracksError,
+                                });
+                            }
+                            tracks = await spotifyItem.resolveYoutubeTracks();
+                            break;
+                        default:
+                            return callback({
+                                status: Response.NoResultsError,
+                            });
+                    }
+                } else {
+                    const results = await bot.music.rest.loadTracks(
+                        /^https?:\/\//.test(query)
+                            ? query
+                            : `ytsearch:${query}`,
+                    );
+                    switch (results.loadType) {
+                        case 'PLAYLIST_LOADED':
+                            tracks = results.tracks;
+                            break;
+                        case 'TRACK_LOADED':
+                        case 'SEARCH_RESULT': {
+                            const [track] = results.tracks;
+                            tracks = [track];
+                            break;
+                        }
+                        case 'NO_MATCHES':
+                            return callback({
+                                status: Response.NoResultsError,
+                            });
+                        case 'LOAD_FAILED':
+                        default:
+                            return callback({ status: Response.GenericError });
+                    }
+                }
+                let player = bot.music.players.get(guildId) as QuaverPlayer;
+                if (!player?.connected) {
+                    player = bot.music.createPlayer(guildId) as QuaverPlayer;
+                    player.handler = new PlayerHandler(bot, player);
+                    player.queue.channel = member.voice
+                        .channel as QuaverChannels;
+                    await player.connect(member.voice.channelId, {
+                        deafened: true,
+                    });
+                    // Ensure that Quaver destroys the player if the user leaves the channel while Quaver is queuing tracks
+                    // Ensure that Quaver destroys the player if Quaver gets timed out by the user while Quaver is queuing tracks
+                    // Ensure that Quaver destroys the player if Quaver gets kicked or banned by the user while Quaver is queuing tracks
+                    me = await bot.guilds.cache.get(guildId).members.fetchMe();
+                    const timedOut = me.isCommunicationDisabled();
+                    if (
+                        !member.voice.channelId ||
+                        timedOut ||
+                        !bot.guilds.cache.get(guildId)
+                    ) {
+                        await player.handler.disconnect();
+                        return callback({ status: Response.GenericError });
+                    }
+                }
+                player.queue.add(tracks, {
+                    requester: socket.user.id,
+                    next: false,
+                });
+                const started = player.playing || player.paused;
+                const smartQueue = await data.guild.get<boolean>(
+                    guildId,
+                    'settings.smartqueue',
+                );
+                if (!started) await player.queue.start();
+                if (smartQueue) await player.handler.sort();
+                if (settings.features.web.enabled) {
+                    io.to(`guild:${guildId}`).emit(
+                        'queueUpdate',
+                        player.queue.tracks.map(
+                            (track: QuaverSong): QuaverSong => {
+                                const user = bot.users.cache.get(
+                                    track.requester,
+                                );
+                                track.requesterTag = user?.tag;
+                                track.requesterAvatar = user?.avatar;
+                                return track;
+                            },
+                        ),
+                    );
+                }
+                break;
+            }
             case UpdateItemType.Loop: {
                 const player = bot.music.players.get(guildId) as QuaverPlayer;
                 if (!player) {
@@ -98,11 +258,11 @@ export default {
                     required: Math.ceil(
                         (
                             await bot.guilds.cache
-                            .get(guildId)
+                                .get(guildId)
                                 .members.fetchMe()
                         ).voice.channel.members.filter(
-                                (m): boolean => !m.user.bot,
-                            ).size / 2,
+                            (m): boolean => !m.user.bot,
+                        ).size / 2,
                     ),
                     users: [],
                 };
@@ -217,7 +377,7 @@ export default {
                 if (
                     !(
                         await bot.guilds.cache
-                        .get(guildId)
+                            .get(guildId)
                             ?.members.fetch(socket.user.id)
                     )?.permissions.has(PermissionsBitField.Flags.ManageGuild)
                 ) {
@@ -258,7 +418,7 @@ export default {
                 if (
                     !(
                         await bot.guilds.cache
-                        .get(guildId)
+                            .get(guildId)
                             ?.members.fetch(socket.user.id)
                     )?.permissions.has(PermissionsBitField.Flags.ManageGuild)
                 ) {
@@ -301,6 +461,7 @@ export default {
 };
 
 export enum UpdateItemType {
+    Add = 'add',
     Loop = 'loop',
     Volume = 'volume',
     Paused = 'paused',
@@ -320,7 +481,12 @@ enum Response {
     AuthenticationError = 'error-auth',
     GenericError = 'error-generic',
     ChannelMismatchError = 'error-channel-mismatch',
+    ChannelUnsupportedError = 'error-channel-unsupported',
     InactiveSessionError = 'error-inactive-session',
     FeatureDisabledError = 'error-feature-disabled',
     FeatureNotWhitelistedError = 'error-feature-not-whitelisted',
+    BotPermissionError = 'error-bot-permission',
+    BotTimedOutError = 'error-bot-timed-out',
+    NoResultsError = 'error-no-results',
+    SpotifyTooManyTracksError = 'error-spotify-too-many-tracks',
 }
