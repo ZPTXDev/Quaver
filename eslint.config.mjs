3d83abce82c053973bfdcf2b462db9260dc12133
Max <max@zptx.dev>
2025-03-30 12:32:29 +0800

    refactor: update eslintrc

---
+++ eslint.config.mjs
@@ -0,0 +1,115 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default defineConfig([globalIgnores(['**/locales/', '**/dist/', '**/scripts/']), {
+    extends: compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ),
+
+    plugins: {
+        '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: 'module',
+    },
+
+    rules: {
+        'arrow-spacing': ['warn', {
+            before: true,
+            after: true,
+        }],
+
+        'brace-style': ['error', '1tbs', {
+            allowSingleLine: true,
+        }],
+
+        'comma-dangle': ['error', 'always-multiline'],
+        'comma-spacing': 'error',
+        'comma-style': 'error',
+        curly: ['error', 'multi-line', 'consistent'],
+        'dot-location': ['error', 'property'],
+        'handle-callback-err': 'off',
+        'keyword-spacing': 'error',
+
+        'max-nested-callbacks': ['error', {
+            max: 4,
+        }],
+
+        'max-statements-per-line': ['error', {
+            max: 2,
+        }],
+
+        'no-console': 'off',
+        'no-empty-function': 'error',
+        'no-floating-decimal': 'error',
+        'no-inline-comments': 'error',
+        'no-lonely-if': 'error',
+        'no-multi-spaces': 'error',
+
+        'no-multiple-empty-lines': ['error', {
+            max: 2,
+            maxEOF: 1,
+            maxBOF: 0,
+        }],
+
+        'no-return-await': 'error',
+        'no-shadow': 'off',
+        'no-trailing-spaces': ['error'],
+        'no-var': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'prefer-const': 'error',
+
+        quotes: ['error', 'single', {
+            avoidEscape: true,
+            allowTemplateLiterals: false,
+        }],
+
+        semi: ['error', 'always'],
+        'space-before-blocks': 'error',
+
+        'space-before-function-paren': ['error', {
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always',
+        }],
+
+        'space-in-parens': 'error',
+        'space-infix-ops': 'error',
+        'space-unary-ops': 'error',
+        'spaced-comment': 'error',
+        yoda: 'error',
+
+        '@typescript-eslint/no-shadow': ['error', {
+            allow: ['err', 'resolve', 'reject'],
+        }],
+
+        '@typescript-eslint/explicit-function-return-type': ['error', {
+            allowTypedFunctionExpressions: false,
+        }],
+
+        '@typescript-eslint/consistent-type-imports': ['error'],
+    },
+}]);
