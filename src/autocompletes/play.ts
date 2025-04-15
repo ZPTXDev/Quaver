9051412f792238cfcba10398561b625a5d89ec48
Max <max@zptx.dev>
2025-04-15 18:08:37 +0800

    feat: improved autocomplete Resolves #1381

--- src/autocompletes/play.ts
+++ src/autocompletes/play.ts
@@ -3,8 +3,15 @@ import type {
     ApplicationCommandOptionChoiceData,
     AutocompleteInteraction,
 } from 'discord.js';
+import { cache, data } from '#src/lib/util/common.js';
 import { request } from 'undici';
-import { YOUTUBE_AUTOCOMPLETE_URL } from '#src/lib/util/constants.js';
+import {
+    acceptableSources,
+    queryOverrides,
+    sourceList,
+    YOUTUBE_AUTOCOMPLETE_URL,
+} from '#src/lib/util/constants.js';
+import { getGuildLocaleString } from '#src/lib/util/util.js';
 
 export default {
     name: 'play',
@@ -12,40 +19,74 @@ export default {
         interaction: QuaverInteraction<AutocompleteInteraction>,
     ): Promise<void> {
         const focused = interaction.options.getFocused();
-        const { body } = await request(`${YOUTUBE_AUTOCOMPLETE_URL}${focused}`);
-        let data;
-        try {
-            data = await body.text();
-            let searchSuggestions: string[] = [];
-            data.split('[').forEach((element: string, index: number): void => {
-                if (!element.split('"')[1] || index === 1) return;
-                searchSuggestions.push(element.split('"')[1]);
+        if (focused === '') return interaction.respond([]);
+        const matchingOverride = queryOverrides.find((q): boolean =>
+            focused.startsWith(q),
+        );
+        const source = matchingOverride
+            ? sourceList[matchingOverride]
+            : ((await data.guild.get<string>(
+                  interaction.guildId,
+                  'settings.source',
+              )) ?? Object.keys(acceptableSources)[0]);
+        const sourceName = await getGuildLocaleString(
+            interaction.guildId,
+            `MISC.SOURCES.${source.toUpperCase()}`,
+        );
+        const query = matchingOverride
+            ? focused.slice(matchingOverride.length)
+            : focused;
+        const existingResults = await cache.get(query.toLowerCase());
+        if (existingResults) {
+            const searchSuggestionsResponse =
+                existingResults as ApplicationCommandOptionChoiceData[];
+            searchSuggestionsResponse.unshift({
+                name: `${sourceName}: ${query}`,
+                value: focused,
             });
+            return interaction.respond(searchSuggestionsResponse);
+        }
+        const { body } = await request(
+            `${YOUTUBE_AUTOCOMPLETE_URL}${query.toLowerCase()}`,
+        );
+        let autocompleteData;
+        try {
+            autocompleteData = await body.text();
+            const searchSuggestions: string[] = [];
+            autocompleteData
+                .split('[')
+                .forEach((element: string, index: number): void => {
+                    if (!element.split('"')[1] || index === 1) return;
+                    searchSuggestions.push(element.split('"')[1]);
+                });
+            // removes the last element, which is a random 'k' in my testing
             searchSuggestions.pop();
-            searchSuggestions = searchSuggestions.filter(
-                (element): boolean => element !== focused,
-            );
-            searchSuggestions.unshift(focused);
-            return await interaction.respond(
-                searchSuggestions
-                    .filter((element): boolean => element !== '')
-                    .map((suggestion): string =>
-                        suggestion
-                            .replace(
-                                /\\u([0-9a-fA-F]{4})/g,
-                                (_whole, grp): string =>
-                                    String.fromCharCode(parseInt(grp, 16)),
-                            )
-                            .slice(0, 100),
-                    )
-                    .map(
-                        (suggestion): ApplicationCommandOptionChoiceData => ({
-                            name: suggestion,
-                            value: suggestion,
-                        }),
-                    )
-                    .slice(0, 25),
-            );
+            const searchSuggestionsResponse = searchSuggestions
+                .filter(
+                    (element): boolean => element !== focused && element !== '',
+                )
+                .map((suggestion): string =>
+                    suggestion
+                        .replace(
+                            /\\u([0-9a-fA-F]{4})/g,
+                            (_whole, grp): string =>
+                                String.fromCharCode(parseInt(grp, 16)),
+                        )
+                        .slice(0, 100),
+                )
+                .slice(0, 24)
+                .map(
+                    (suggestion): ApplicationCommandOptionChoiceData => ({
+                        name: suggestion,
+                        value: suggestion,
+                    }),
+                );
+            await cache.set(query.toLowerCase(), searchSuggestionsResponse);
+            searchSuggestionsResponse.unshift({
+                name: `${sourceName}: ${query}`,
+                value: focused,
+            });
+            return await interaction.respond(searchSuggestionsResponse);
         } catch {
             return interaction.respond([]);
         }
--- src/autocompletes/search.ts
+++ src/autocompletes/search.ts
@@ -5,6 +5,7 @@ import type {
 } from 'discord.js';
 import { request } from 'undici';
 import { YOUTUBE_AUTOCOMPLETE_URL } from '#src/lib/util/constants.js';
+import { cache } from '#src/lib/util/common.js';
 
 export default {
     name: 'search',
@@ -12,40 +13,58 @@ export default {
         interaction: QuaverInteraction<AutocompleteInteraction>,
     ): Promise<void> {
         const focused = interaction.options.getFocused();
-        const { body } = await request(`${YOUTUBE_AUTOCOMPLETE_URL}${focused}`);
-        let data;
-        try {
-            data = await body.text();
-            let searchSuggestions: string[] = [];
-            data.split('[').forEach((element: string, index: number): void => {
-                if (!element.split('"')[1] || index === 1) return;
-                searchSuggestions.push(element.split('"')[1]);
+        if (focused === '') return interaction.respond([]);
+        const existingResults = await cache.get(focused.toLowerCase());
+        if (existingResults) {
+            const searchSuggestionsResponse =
+                existingResults as ApplicationCommandOptionChoiceData[];
+            searchSuggestionsResponse.unshift({
+                name: focused,
+                value: focused,
             });
+            return interaction.respond(searchSuggestionsResponse);
+        }
+        const { body } = await request(
+            `${YOUTUBE_AUTOCOMPLETE_URL}${focused.toLowerCase()}`,
+        );
+        let autocompleteData;
+        try {
+            autocompleteData = await body.text();
+            const searchSuggestions: string[] = [];
+            autocompleteData
+                .split('[')
+                .forEach((element: string, index: number): void => {
+                    if (!element.split('"')[1] || index === 1) return;
+                    searchSuggestions.push(element.split('"')[1]);
+                });
+            // removes the last element, which is a random 'k' in my testing
             searchSuggestions.pop();
-            searchSuggestions = searchSuggestions.filter(
-                (element): boolean => element !== focused,
-            );
-            searchSuggestions.unshift(focused);
-            return await interaction.respond(
-                searchSuggestions
-                    .filter((element): boolean => element !== '')
-                    .map((suggestion): string =>
-                        suggestion
-                            .replace(
-                                /\\u([0-9a-fA-F]{4})/g,
-                                (_whole, grp): string =>
-                                    String.fromCharCode(parseInt(grp, 16)),
-                            )
-                            .slice(0, 100),
-                    )
-                    .map(
-                        (suggestion): ApplicationCommandOptionChoiceData => ({
-                            name: suggestion,
-                            value: suggestion,
-                        }),
-                    )
-                    .slice(0, 25),
-            );
+            const searchSuggestionsResponse = searchSuggestions
+                .filter(
+                    (element): boolean => element !== focused && element !== '',
+                )
+                .map((suggestion): string =>
+                    suggestion
+                        .replace(
+                            /\\u([0-9a-fA-F]{4})/g,
+                            (_whole, grp): string =>
+                                String.fromCharCode(parseInt(grp, 16)),
+                        )
+                        .slice(0, 100),
+                )
+                .slice(0, 24)
+                .map(
+                    (suggestion): ApplicationCommandOptionChoiceData => ({
+                        name: suggestion,
+                        value: suggestion,
+                    }),
+                );
+            await cache.set(focused.toLowerCase(), searchSuggestionsResponse);
+            searchSuggestionsResponse.unshift({
+                name: focused,
+                value: focused,
+            });
+            return await interaction.respond(searchSuggestionsResponse);
         } catch {
             return interaction.respond([]);
         }
