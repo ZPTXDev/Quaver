1245022c20855cc0fa0cb6bb0531b2db2e580a24
xayanide <xayanide@gmail.com>
2025-11-24 02:07:42 +0800

    chore: update and simplify eslint.config.mjs to follow new flatConfig
    Adds the following devDependencies:
    - `typescript-eslint`
    - `eslint-config-prettier`
    
    Removes the following devDependencies:
    - `@typescript-eslint/eslint-plugin`
    - `@typescript-eslint/parser`
    
    Removed `languageOptions`:
    - `parser`
    - `sourceType`
    
    Because they are already provided by `tsEslint.configs.recommended`.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,114 +1,146 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
-
-export default defineConfig([globalIgnores(['**/locales/', '**/dist/', '**/scripts/']), {
-    extends: compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-    ),
-
-    plugins: {
-        '@typescript-eslint': typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
-        },
-
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: 'module',
-    },
-
-    rules: {
-        'arrow-spacing': ['warn', {
+const preferredRules = {
+    'arrow-spacing': [
+        'warn',
+        {
             before: true,
             after: true,
-        }],
+        },
+    ],
 
-        'brace-style': ['error', '1tbs', {
+    'brace-style': [
+        'error',
+        '1tbs',
+        {
             allowSingleLine: true,
-        }],
+        },
+    ],
 
-        'comma-dangle': ['error', 'always-multiline'],
-        'comma-spacing': 'error',
-        'comma-style': 'error',
-        curly: ['error', 'multi-line', 'consistent'],
-        'dot-location': ['error', 'property'],
-        'handle-callback-err': 'off',
-        'keyword-spacing': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    curly: ['error', 'multi-line', 'consistent'],
+    'dot-location': ['error', 'property'],
+    'handle-callback-err': 'off',
+    'keyword-spacing': 'error',
 
-        'max-nested-callbacks': ['error', {
+    'max-nested-callbacks': [
+        'error',
+        {
             max: 4,
-        }],
+        },
+    ],
 
-        'max-statements-per-line': ['error', {
+    'max-statements-per-line': [
+        'error',
+        {
             max: 2,
-        }],
+        },
+    ],
 
-        'no-console': 'off',
-        'no-empty-function': 'error',
-        'no-floating-decimal': 'error',
-        'no-inline-comments': 'error',
-        'no-lonely-if': 'error',
-        'no-multi-spaces': 'error',
+    'no-console': 'off',
+    'no-empty-function': 'error',
+    'no-floating-decimal': 'error',
+    'no-inline-comments': 'error',
+    'no-lonely-if': 'error',
+    'no-multi-spaces': 'error',
 
-        'no-multiple-empty-lines': ['error', {
+    'no-multiple-empty-lines': [
+        'error',
+        {
             max: 2,
             maxEOF: 1,
             maxBOF: 0,
-        }],
+        },
+    ],
 
-        'no-shadow': 'off',
-        'no-trailing-spaces': ['error'],
-        'no-var': 'error',
-        'object-curly-spacing': ['error', 'always'],
-        'prefer-const': 'error',
+    'no-shadow': 'off',
+    'no-trailing-spaces': ['error'],
+    'no-var': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-const': 'error',
 
-        quotes: ['error', 'single', {
+    quotes: [
+        'error',
+        'single',
+        {
             avoidEscape: true,
             allowTemplateLiterals: false,
-        }],
+        },
+    ],
 
-        semi: ['error', 'always'],
-        'space-before-blocks': 'error',
+    semi: ['error', 'always'],
+    'space-before-blocks': 'error',
 
-        'space-before-function-paren': ['error', {
-            anonymous: 'never',
+    'space-before-function-paren': [
+        'error',
+        {
+            anonymous: 'always',
             named: 'never',
             asyncArrow: 'always',
-        }],
+            catch: 'always',
+        },
+    ],
 
-        'space-in-parens': 'error',
-        'space-infix-ops': 'error',
-        'space-unary-ops': 'error',
-        'spaced-comment': 'error',
-        yoda: 'error',
+    'space-in-parens': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'spaced-comment': 'error',
+    yoda: 'error',
 
-        '@typescript-eslint/no-shadow': ['error', {
+    '@typescript-eslint/no-shadow': [
+        'error',
+        {
             allow: ['err', 'resolve', 'reject'],
-        }],
+        },
+    ],
 
-        '@typescript-eslint/explicit-function-return-type': ['error', {
+    '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
             allowTypedFunctionExpressions: false,
-        }],
+        },
+    ],
 
-        '@typescript-eslint/consistent-type-imports': ['error'],
-    },
-}]);
+    '@typescript-eslint/consistent-type-imports': ['error'],
+};
+
+const reinforcedRules = {
+    quotes: [
+        'error',
+        'single',
+        { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+};
+
+export default defineConfig(
+    // ESLint recommended config only contains rules. This'll be our base rules
+    eslint.configs.recommended,
+    // TSESLint recommended config handles rules, files, languageOptions: parser, plugin
+    tsEslint.configs.recommended,
+    [
+        globalIgnores(['**/locales/', '**/dist/', '**/scripts/']),
+        {
+            languageOptions: {
+                globals: {
+                    ...globals.node,
+                    ...globals.es2022,
+                },
+                ecmaVersion: 2022,
+            },
+            rules: {
+                // Override ESLint and TSESLint rules with our preferred rules
+                ...preferredRules,
+                // Lastly, let prettier override rules that may conflict with the rules above
+                ...eslintConfigPrettier.rules,
+                // Rules we don't want to be overriden no matter what rules are set by other configs
+                ...reinforcedRules,
+            },
+        },
+    ],
+);
