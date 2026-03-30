49157df4058abd20b54d047c61fc802f247941c0
Max <max@zptx.dev>
2026-03-30 13:15:14 +0800

    fix: add return statement

--- src/lib/settings/PlaybackLogicHandler.ts
+++ src/lib/settings/PlaybackLogicHandler.ts
@@ -149,6 +149,7 @@ export class PlaybackLogicHandler {
                     ),
                     { force: ForceType.Update },
                 );
+                return;
             }
         }
     }
